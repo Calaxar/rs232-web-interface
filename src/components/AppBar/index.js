e9e0e0ce170fb23ec1297299f0999cfca7d23f42
Calum Hay <calumhay@gmail.com>
2021-12-29 12:51:14 +1100

    Created background and app bar

---
+++ src/components/AppBar/index.js
@@ -0,0 +1,12 @@
+
+import './AppBar.css';
+
+const AppBar = () => {
+	return (
+		<div className="AppBar">
+			<span className="AppTitle">RS-232 Controller</span>
+		</div>
+	);
+};
+
+export default AppBar;
